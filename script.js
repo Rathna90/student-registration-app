8e788643ae6296af2d88776e5736cc770cff52a6
Rathna90 <s.rathnamca@gmail.com>
2026-06-01 08:09:29 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function registerStudent(){
+
+    let name=document.getElementById("name").value;
+    let email=document.getElementById("email").value;
+    let course=document.getElementById("course").value;
+
+    document.getElementById("msg").innerHTML=
+    "Registration Successful";
+}
+
+
