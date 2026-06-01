8e53803c349bc5c70ad855cdfd8a98e00d0efa46
Rathna90 <s.rathnamca@gmail.com>
2026-06-01 10:52:24 +0530

    Debug

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 async function registerStudent() {
 
-    alert("Function Called");
+    alert("STEP 1");
 
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let course = document.getElementById("course").value;
+
+    alert("STEP 2");
 
     const response = await fetch(
         "http://65.0.95.93:3000/register",
@@ -20,6 +22,8 @@ async function registerStudent() {
             })
         }
     );
+
+    alert("STEP 3");
 
     const data = await response.json();
 
