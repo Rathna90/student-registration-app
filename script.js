2c432b21fb6d459f6f80673d200de6ab4fb5e10a
Rathna90 <s.rathnamca@gmail.com>
2026-06-01 10:32:22 +0530

    Debug fetch

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 async function registerStudent() {
 
+    alert("Function Called");
+
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let course = document.getElementById("course").value;
